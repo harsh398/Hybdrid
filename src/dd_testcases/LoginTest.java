1695a03f89418c49cbe8a0725eec3d8971a9925a
harsharan <harsh398@live.com>
2018-03-02 21:42:32 +0530

    Initial Commit

---
+++ src/dd_testcases/LoginTest.java
@@ -0,0 +1,63 @@
+package dd_testcases;
+
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.testng.SkipException;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import dd_core.Page;
+import dd_util.TestUtil;
+
+
+public class LoginTest extends Page {
+
+	
+	public static String GlobalUsername;
+	
+	@BeforeTest
+	public void isSkip(){
+		
+		if(!TestUtil.isExecutable("LoginTest")){
+			
+			throw new SkipException("Skipping the test as the Run mode is No");
+			
+		}
+		
+		
+	}
+	
+	@Test(dataProvider="getData")
+	public void doLogin(String username,String password) throws IOException{
+		
+		logs.debug("Inside Login Test");
+		System.out.print("Started the Test");
+		findElement("username").sendKeys(username);
+		System.out.print("Entered USername");
+		findElement("password").sendKeys(password);
+		System.out.print("Entered Password");
+		findElement("login").click();
+		System.out.print("Clicked Login");
+		TestUtil.CaptureScreenshot();
+		GlobalUsername = username;
+		
+	
+	}
+	
+	
+	@DataProvider
+	public static Object[][] getData(){
+		
+		return TestUtil.getData("LoginTest");
+		
+		
+		
+	}
+	
+	
+	
+	
+}
